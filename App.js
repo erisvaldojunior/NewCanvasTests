929a41772fe6940703181141062a389d245eb863
Erisvaldo <erisvaldojunior@gmail.com>
2021-08-26 08:54:39 -0300

    GCanvas tests

---
+++ App.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+import CanvasExample from './src/CanvasExample';
+
+const App = () => {
+  return <CanvasExample />;
+};
+
+export default App;
